a353da79116e9ba429fecba720d4fab29aea0125
Caner Egin <cnrgn96@gmail.com>
2019-12-25 14:48:18 +0300

    Updates

---
+++ precache-manifest.396a0d4d2b170949e44d50a772b024ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d324c3c5b2807959f3ba7b56d9706a9",
+    "url": "/aibird/index.html"
+  },
+  {
+    "revision": "a2c0a070b287015935b1",
+    "url": "/aibird/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7b642c37fde74ba5cf98",
+    "url": "/aibird/static/js/2.a8eabf94.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/aibird/static/js/2.a8eabf94.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a2c0a070b287015935b1",
+    "url": "/aibird/static/js/main.e13712c8.chunk.js"
+  },
+  {
+    "revision": "4d9bc718797dc44e0980",
+    "url": "/aibird/static/js/runtime-main.f13243e4.js"
+  }
+]);
